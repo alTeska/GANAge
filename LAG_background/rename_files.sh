c8307713dd404d4cfd83209118a9d33abe072d70
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-02-02 20:05:28 +0100

    old data removed

--- LAG_background/rename_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-foldername=$1
-cd $1
-
-find -depth -name '* *' | rename 's/ /_/g'
-for i in `ls`;do count=1;for j in `ls $i/*.png`;do mv $j $i/$count.png;count=$((count+1));done;done
-for i in `ls`;do count=1;for j in `ls $i/y/*.png`;do mv $j $i/y/$count.png;count=$((count+1));done;done
-
